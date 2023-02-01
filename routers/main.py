8c71727eaad2e9f79a2402035e633d535d291264
John <is.boldyrev@yandex.ru>
2023-02-01 23:41:11 +0300

    добавлено завершение парсинга

--- routers/main.py
+++ routers/main.py
@@ -112,3 +112,32 @@ async def start_parse(message: types.Message):
         text=texts.choose_parser,
         reply_markup=parsers_markup
     )
+
+
+@router.message(Text(text_contains="завершить парсинг", text_ignore_case=True), state="*")
+async def parsing_end_handler(message: types.Message, state: FSMContext):
+    curr_state = await state.get_state()
+    if curr_state:
+        if curr_state.split(":")[1] == "cancel":
+            await state.clear()
+            return
+
+    full_name = message.from_user.first_name
+    if message.from_user.last_name:
+        full_name += " " + message.from_user.last_name
+    try:
+        User.get(user_id=message.from_user.id)
+    except User.DoesNotExist:
+        User.create(
+            user_id=message.from_user.id,
+            full_name=full_name,
+            username=message.from_user.username
+        )
+    await message.answer(
+        text=texts.welcome.format(
+            user_id=message.from_user.id,
+            full_name=full_name
+        ),
+        reply_markup=menu_markup
+    )
+    await state.clear()
